b3f1266601519c481f90925412988c9d860a07cc
bagaswihant <bagaswihant@gmail.com>
2024-08-01 14:11:25 +0700

    fix insert new item in general text

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GeneralText;
 use App\Models\Project;
 use App\Models\ProjectStack;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PortfolioController extends Controller
@@ -12,13 +14,27 @@ class PortfolioController extends Controller
     public function index()
     {
         $data = Project::with(['stacks'])->get();
-        
+
         // foreach ($data as $value) {
         //     foreach ($value->stacks as $stack) {
         //         dump($stack->name);
         //     }
         // }
-        
-        return Inertia::render('Dashboard', ['data' => $data]);
+
+        return Inertia::render('GeneralText', ['data' => $data]);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            GeneralText::create($request->only([
+                'title_en',
+                'title_id', 'sub_title_en', 'sub_title_id', 'small_sub_title_en', 'small_sub_title_id', 'description_en', 'description_id', 'contact_text_en', 'contact_text_id'
+            ]));
+            return response(json_encode(['message' => 'Success']), 200);
+        } catch (\Throwable $th) {
+            return response(json_encode(['message'=>$th->getMessage()]), 500);
+        }
+
     }
 }
--- app/Models/GeneralText.php
+++ app/Models/GeneralText.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class GeneralText extends Model
 {
     use HasFactory;
-
+    public $timestamps = false;
+    public $fillable =['title_en','title_id','sub_title_en','sub_title_id','small_sub_title_en','small_sub_title_id','description_en','description_id','contact_text_en','contact_text_id'];
     public function listText()  {
         return $this->belongsToMany(ListText::class);
     }
