f11d491e933dfa1e2b8e3af919ad615f63840a90
bagaswihant <bagaswihant@gmail.com>
2024-08-07 22:33:07 +0700

    fix response api

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ApiResource;
 use App\Models\GeneralText;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     public function getDataPortfolio()
     {
-        $data = GeneralText::with('listText')->get();
-        return response(json_encode(['text'=>$data]), 200);
+        $text = GeneralText::with('listText')->get();
+        $project = Project::with('stacks')->get();
+        $res = ['text'=>$text,'project'=>$project];
+        return ApiResource::collection($res);
+        
     }
 }
